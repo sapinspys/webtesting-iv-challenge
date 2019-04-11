19ad9afc07a6547abf561b46f76bf8839fed3377
Guillermo Arria-Devoe <garri009@fiu.edu>
2019-04-11 12:05:11 -0400

    Configured knexfile for development

--- knexfile.js
+++ knexfile.js
@@ -5,7 +5,14 @@ module.exports = {
   development: {
     client: 'sqlite3',
     connection: {
-      filename: './dev.sqlite3'
+      filename: './data/database.sqlite3'
+    },
+    useNullAsDefault: true,
+    migrations: {
+      directry: './data/migrations'
+    },
+    seeds: {
+      directory: './data/seeds'
     }
   },
 
