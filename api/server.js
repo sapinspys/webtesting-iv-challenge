ab6d105f4cc0709de7260780d24e9d49f7ed4fd5
Guillermo Arria-Devoe <garri009@fiu.edu>
2019-04-11 12:14:12 -0400

    Configured cors

--- api/server.js
+++ api/server.js
@@ -1,10 +1,13 @@
 const express = require('express');
+const cors = require('cors');
 
 // const model = require('../model/modelModel.js');
 
 const server = express();
 
 server.use(express.json());
+server.use(cors());
+
 
 server.get('/', async (req, res) => {
   res.status(200).json({ api: 'up' });
