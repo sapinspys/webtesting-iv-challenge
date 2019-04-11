77da03997babf992e08a149da8a4cc06115d212d
Guillermo Arria-Devoe <garri009@fiu.edu>
2019-04-11 18:18:04 -0400

    Created users model helper file

--- index.js
+++ index.js
@@ -4,5 +4,5 @@ const server = require("./api/server.js");
 
 const port = process.env.PORT || 5000;
 server.listen(port, () =>
-  console.log(`\n** Server running on http://localhost:${port}`)
+  console.log(`\n** Server running on http://localhost:${port} **\n`)
 );
